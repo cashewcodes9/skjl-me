157207163f03c98bc426a470fc3fb722d1a6ba6a
rohit-private <rohit-private@rohits-imac.speedport_w_724v_typ_a_05011603_06_003>
2021-09-29 23:04:34 +0200

    first version website

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Button } from '../ButtonElements';
+
+import Video from '../../videos/video.mp4';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements';
+
+function HeroSection() {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Tokenize your future calender on blockchain</HeroH1>
+        <HeroP>
+          Sell your calender time slots for your services instantly. No banking integration needed.
+          
+          Sign up for as beta user and get 3 months free.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default HeroSection;
